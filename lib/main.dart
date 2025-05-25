44ae9f186f064022f57e06215727a677ba40326e
Saiyam Kalra <kalrasaiyam2006@gmail.com>
2025-05-26 00:54:58 +0530

    authentication completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:social_media_app/authentication/login_or_register.dart';
+import 'package:social_media_app/authentication/auth_page.dart';
+
 import 'package:social_media_app/firebase_options.dart';
 import 'package:social_media_app/theme/dark_mode.dart';
 import 'package:social_media_app/theme/light_mode.dart';
@@ -18,7 +19,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginOrRegisterPage(),
+      home: AuthPage(),
       theme: lightMode,
       darkTheme: darkMode,
     );
--- lib/mainApp/home_page.dart
+++ lib/mainApp/home_page.dart
@@ -1,10 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+  void logout() {
+    FirebaseAuth.instance.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              logout();
+            },
+            icon: Icon(Icons.logout),
+          ),
+        ],
+      ),
+    );
   }
 }
