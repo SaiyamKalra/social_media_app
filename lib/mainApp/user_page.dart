e1cd9e964d7e57c8e84683b9b5546b3936ebbdc9
Saiyam Kalra <kalrasaiyam2006@gmail.com>
2025-05-26 13:24:54 +0530

    completed the user page

--- lib/mainApp/user_page.dart
+++ lib/mainApp/user_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class UsersPage extends StatelessWidget {
@@ -7,8 +8,46 @@ class UsersPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text('Users')),
+        title: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 110.0),
+          child: Text('Users'),
+        ),
         backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("Users").snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            showDialog(
+              context: context,
+              builder:
+                  (context) => AlertDialog(title: Text("Something went wrong")),
+            );
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.data == null) {
+            return Text("No Data");
+          }
+
+          final users = snapshot.data!.docs;
+
+          return Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: users.length,
+                  itemBuilder: (context, index) {
+                    final user = users[index];
+                    return ListTile(
+                      title: Text(user['username']),
+                      subtitle: Text(user['email']),
+                    );
+                  },
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
